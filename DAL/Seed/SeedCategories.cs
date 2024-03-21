04ca1378b41272436c0017e9721c6901ca41364a
bora yıldırım <borayildirim222@gmail.com>
2024-03-21 19:33:08 +0300

    ID degisikligi,SeedFoods

--- DAL/Seed/SeedCategories.cs
+++ DAL/Seed/SeedCategories.cs
@@ -16,17 +16,17 @@ namespace DAL.Seed
             builder.HasData
                 (
                   
-                    new Category { CategoryID = 2, CategoryName = "Fruit" },
-                    new Category { CategoryID = 3, CategoryName = "Drink" },
-                    new Category { CategoryID = 5, CategoryName = "Snack" },
-                    new Category { CategoryID = 6, CategoryName = "Dessert" },
-                    new Category { CategoryID = 7, CategoryName = "Salad" },
-                    new Category { CategoryID = 8, CategoryName = "Dairy" },
-                    new Category { CategoryID = 9, CategoryName = "Fish and Seafood" },
-                    new Category { CategoryID = 10, CategoryName = "Fast Food" },
-                    new Category { CategoryID = 11, CategoryName = "Meat or Poultry" },
-                    new Category { CategoryID = 12, CategoryName = "Grain, Bean and Nuts" },
-                    new Category { CategoryID = 13, CategoryName = "Others" }
+                    new Category { CategoryID = 1, CategoryName = "Fruit" },
+                    new Category { CategoryID = 2, CategoryName = "Drink" },
+                    new Category { CategoryID = 3, CategoryName = "Snack" },
+                    new Category { CategoryID = 4, CategoryName = "Dessert" },
+                    new Category { CategoryID = 5, CategoryName = "Salad" },
+                    new Category { CategoryID = 6, CategoryName = "Dairy" },
+                    new Category { CategoryID = 7, CategoryName = "Fish and Seafood" },
+                    new Category { CategoryID = 8, CategoryName = "Fast Food" },
+                    new Category { CategoryID = 9, CategoryName = "Meat or Poultry" },
+                    new Category { CategoryID = 10, CategoryName = "Grain, Bean and Nuts" },
+                    new Category { CategoryID = 11, CategoryName = "Others" }
                 );
         }
     }
