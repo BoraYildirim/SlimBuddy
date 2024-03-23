827667d6719a6b743562474a688605312145dfed
bora yıldırım <borayildirim222@gmail.com>
2024-03-23 22:18:58 +0300

    gggddd

--- PL/CreateaAccountScreen.cs
+++ PL/CreateaAccountScreen.cs
@@ -1,5 +1,4 @@
 ﻿using BLL;
-using DAL.Repositories.Concrete;
 using Models.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace PL
 {
@@ -33,23 +33,95 @@ namespace PL
             }
             else
             {
-                MessageBox.Show("Hatalı Kullanıcı Adı girdiniz...");
+                MessageBox.Show("Wrong Email address...");
                 textBoxEmail.Text = string.Empty;
             }
         }
 
-     
+        public void PasswordControl(string password, User user)
+        {
+            int letterControl = 0;
+            int digitControl = 0;
+            int symbolConrol = 0;
+
+            foreach (char c in password)
+            {
+
+                if (char.IsLetter(c))
+                {
+                    letterControl++;
+                }
+                if (char.IsDigit(c))
+                {
+                    digitControl++;
+                }
+                if (char.IsSymbol(c) || char.IsSeparator(c) || char.IsPunctuation(c))
+                {
+                    symbolConrol++;
+                }
+            }
+            if (letterControl > 0 && digitControl >0 && symbolConrol>0)
+            {
+                user.Password = password;
+            }
+            else
+            {
+                MessageBox.Show("Password must have minimum 1-symbol,1-digit and 1-Punction");
+            }
+
+
+        }
+
+        public void NameControl(string name,UserDetail userDetail)
+        {
+            int letterControl = 0;
+            int digitControl = 0;
+            int symbolConrol = 0;
+
+            foreach (char c in name)
+            {
+
+                if (char.IsLetter(c))
+                {
+                    letterControl++;
+                }
+                if (char.IsDigit(c))
+                {
+                    digitControl++;
+                }
+                if (char.IsSymbol(c) || char.IsSeparator(c) || char.IsPunctuation(c))
+                {
+                    symbolConrol++;
+                }
+            }
+            if (letterControl > 0 && digitControl == 0 && symbolConrol == 0)
+            {
+               userDetail.Name=name;
+            }
+            else
+            {
+                MessageBox.Show("Name must have minimum 1-symbol,1-digit and 1-Punction");
+            }
+        }
+
 
 
         private void buttonRegister_Click(object sender, EventArgs e)
         {
             User user = new User();
+            UserDetail userDetail = new UserDetail();
+            userDetail.User = user;
 
-            EmailControl(textBoxEmail.Text,user);
-            
+            EmailControl(textBoxEmail.Text, user);
+            PasswordControl(textBoxPassword.Text,user);
+            NameControl(textBoxName.Text, userDetail);
            
+
         }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
 
+        }
     }
 }
