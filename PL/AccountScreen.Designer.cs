b74718ed8dac9737f1a3bbe49995023552ff1271
bora yıldırım <borayildirim222@gmail.com>
2024-03-23 15:56:03 +0300

    accountscreen

--- PL/AccountScreen.Designer.cs
+++ PL/AccountScreen.Designer.cs
@@ -35,82 +35,288 @@
             label3 = new Label();
             label2 = new Label();
             label4 = new Label();
+            label1 = new Label();
+            label5 = new Label();
+            label10 = new Label();
+            txtNewAge = new TextBox();
+            txtNewHeight = new TextBox();
+            txtNewWeight = new TextBox();
+            label11 = new Label();
+            lblEmail = new Label();
+            lblFirstName = new Label();
+            lblLastName = new Label();
+            lblGender = new Label();
+            lblAge = new Label();
+            lblHeight = new Label();
+            lblWeight = new Label();
+            button2 = new Button();
+            button1 = new Button();
+            btnUpdate = new Button();
             SuspendLayout();
             // 
             // label7
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
-            label7.Location = new Point(345, 268);
+            label7.Location = new Point(62, 239);
             label7.Name = "label7";
-            label7.Size = new Size(164, 39);
+            label7.Size = new Size(188, 39);
             label7.TabIndex = 31;
-            label7.Text = "Last Name";
+            label7.Text = "Last Name :";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(789, 357);
+            label6.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
+            label6.Location = new Point(564, 137);
             label6.Name = "label6";
-            label6.Size = new Size(56, 20);
+            label6.Size = new Size(138, 39);
             label6.TabIndex = 30;
-            label6.Text = "Weight";
+            label6.Text = "Weight :";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(791, 268);
+            label8.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
+            label8.Location = new Point(564, 64);
             label8.Name = "label8";
-            label8.Size = new Size(54, 20);
+            label8.Size = new Size(131, 39);
             label8.TabIndex = 29;
-            label8.Text = "Height";
+            label8.Text = "Height :";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(800, 177);
+            label9.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
+            label9.Location = new Point(62, 410);
             label9.Name = "label9";
-            label9.Size = new Size(36, 20);
+            label9.Size = new Size(92, 39);
             label9.TabIndex = 28;
-            label9.Text = "Age";
+            label9.Text = "Age :";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.BackColor = SystemColors.Menu;
             label3.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
-            label3.Location = new Point(345, 177);
+            label3.Location = new Point(62, 149);
             label3.Name = "label3";
-            label3.Size = new Size(171, 39);
+            label3.Size = new Size(195, 39);
             label3.TabIndex = 25;
-            label3.Text = "First Name";
+            label3.Text = "First Name :";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.BackColor = SystemColors.Menu;
             label2.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
-            label2.Location = new Point(70, 65);
+            label2.Location = new Point(62, 64);
             label2.Name = "label2";
-            label2.Size = new Size(108, 39);
+            label2.Size = new Size(132, 39);
             label2.TabIndex = 24;
-            label2.Text = "E-Mail";
+            label2.Text = "E-Mail :";
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
-            label4.Location = new Point(397, 369);
+            label4.Location = new Point(62, 323);
             label4.Name = "label4";
-            label4.Size = new Size(112, 39);
+            label4.Size = new Size(136, 39);
             label4.TabIndex = 27;
-            label4.Text = "Gender";
+            label4.Text = "Gender :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
+            label1.Location = new Point(738, 430);
+            label1.Name = "label1";
+            label1.Size = new Size(114, 39);
+            label1.TabIndex = 34;
+            label1.Text = "Weight";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
+            label5.Location = new Point(738, 357);
+            label5.Name = "label5";
+            label5.Size = new Size(107, 39);
+            label5.TabIndex = 33;
+            label5.Text = "Height";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
+            label10.Location = new Point(738, 287);
+            label10.Name = "label10";
+            label10.Size = new Size(68, 39);
+            label10.TabIndex = 32;
+            label10.Text = "Age";
+            // 
+            // txtNewAge
+            // 
+            txtNewAge.Location = new Point(892, 299);
+            txtNewAge.Name = "txtNewAge";
+            txtNewAge.Size = new Size(185, 27);
+            txtNewAge.TabIndex = 35;
+            // 
+            // txtNewHeight
+            // 
+            txtNewHeight.Location = new Point(892, 369);
+            txtNewHeight.Name = "txtNewHeight";
+            txtNewHeight.Size = new Size(185, 27);
+            txtNewHeight.TabIndex = 36;
+            // 
+            // txtNewWeight
+            // 
+            txtNewWeight.Location = new Point(892, 441);
+            txtNewWeight.Name = "txtNewWeight";
+            txtNewWeight.Size = new Size(185, 27);
+            txtNewWeight.TabIndex = 37;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Comic Sans MS", 16.2F, FontStyle.Bold);
+            label11.ForeColor = Color.Red;
+            label11.Location = new Point(564, 287);
+            label11.Name = "label11";
+            label11.Size = new Size(141, 39);
+            label11.TabIndex = 38;
+            label11.Text = "UPDATE:";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.BackColor = SystemColors.Menu;
+            lblEmail.Font = new Font("Comic Sans MS", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            lblEmail.Location = new Point(210, 67);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(41, 31);
+            lblEmail.TabIndex = 39;
+            lblEmail.Text = "---";
+            // 
+            // lblFirstName
+            // 
+            lblFirstName.AutoSize = true;
+            lblFirstName.BackColor = SystemColors.Menu;
+            lblFirstName.Font = new Font("Comic Sans MS", 13.8F);
+            lblFirstName.Location = new Point(270, 155);
+            lblFirstName.Name = "lblFirstName";
+            lblFirstName.Size = new Size(41, 31);
+            lblFirstName.TabIndex = 40;
+            lblFirstName.Text = "---";
+            // 
+            // lblLastName
+            // 
+            lblLastName.AutoSize = true;
+            lblLastName.BackColor = SystemColors.Menu;
+            lblLastName.Font = new Font("Comic Sans MS", 13.8F);
+            lblLastName.Location = new Point(274, 245);
+            lblLastName.Name = "lblLastName";
+            lblLastName.Size = new Size(41, 31);
+            lblLastName.TabIndex = 41;
+            lblLastName.Text = "---";
+            // 
+            // lblGender
+            // 
+            lblGender.AutoSize = true;
+            lblGender.BackColor = SystemColors.Menu;
+            lblGender.Font = new Font("Comic Sans MS", 13.8F);
+            lblGender.Location = new Point(239, 329);
+            lblGender.Name = "lblGender";
+            lblGender.Size = new Size(41, 31);
+            lblGender.TabIndex = 42;
+            lblGender.Text = "---";
+            // 
+            // lblAge
+            // 
+            lblAge.AutoSize = true;
+            lblAge.BackColor = SystemColors.Menu;
+            lblAge.Font = new Font("Comic Sans MS", 13.8F);
+            lblAge.Location = new Point(188, 416);
+            lblAge.Name = "lblAge";
+            lblAge.Size = new Size(41, 31);
+            lblAge.TabIndex = 43;
+            lblAge.Text = "---";
+            // 
+            // lblHeight
+            // 
+            lblHeight.AutoSize = true;
+            lblHeight.BackColor = SystemColors.Menu;
+            lblHeight.Font = new Font("Comic Sans MS", 13.8F);
+            lblHeight.Location = new Point(729, 70);
+            lblHeight.Name = "lblHeight";
+            lblHeight.Size = new Size(41, 31);
+            lblHeight.TabIndex = 44;
+            lblHeight.Text = "---";
+            // 
+            // lblWeight
+            // 
+            lblWeight.AutoSize = true;
+            lblWeight.BackColor = SystemColors.Menu;
+            lblWeight.Font = new Font("Comic Sans MS", 13.8F);
+            lblWeight.Location = new Point(729, 143);
+            lblWeight.Name = "lblWeight";
+            lblWeight.Size = new Size(41, 31);
+            lblWeight.TabIndex = 45;
+            lblWeight.Text = "---";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Enabled = false;
+            button2.Location = new Point(452, 64);
+            button2.Name = "button2";
+            button2.Size = new Size(10, 404);
+            button2.TabIndex = 46;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Enabled = false;
+            button1.Location = new Point(564, 224);
+            button1.Name = "button1";
+            button1.Size = new Size(513, 11);
+            button1.TabIndex = 47;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.BackColor = Color.IndianRed;
+            btnUpdate.Location = new Point(564, 357);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(131, 78);
+            btnUpdate.TabIndex = 48;
+            btnUpdate.Text = "UPDATE";
+            btnUpdate.UseVisualStyleBackColor = false;
             // 
             // AccountScreen
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1168, 558);
+            Controls.Add(btnUpdate);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(lblWeight);
+            Controls.Add(lblHeight);
+            Controls.Add(lblAge);
+            Controls.Add(lblGender);
+            Controls.Add(lblLastName);
+            Controls.Add(lblFirstName);
+            Controls.Add(lblEmail);
+            Controls.Add(label11);
+            Controls.Add(txtNewWeight);
+            Controls.Add(txtNewHeight);
+            Controls.Add(txtNewAge);
+            Controls.Add(label1);
+            Controls.Add(label5);
+            Controls.Add(label10);
             Controls.Add(label7);
             Controls.Add(label6);
             Controls.Add(label8);
@@ -133,5 +339,22 @@
         private Label label3;
         private Label label2;
         private Label label4;
+        private Label label1;
+        private Label label5;
+        private Label label10;
+        private TextBox txtNewAge;
+        private TextBox txtNewHeight;
+        private TextBox txtNewWeight;
+        private Label label11;
+        private Label lblEmail;
+        private Label lblFirstName;
+        private Label lblLastName;
+        private Label lblGender;
+        private Label lblAge;
+        private Label lblHeight;
+        private Label lblWeight;
+        private Button button2;
+        private Button button1;
+        private Button btnUpdate;
     }
 }
