b3878e3fdf761ceb02f245be0a70702ed2d2ce5a
bora yıldırım <borayildirim222@gmail.com>
2024-03-25 16:02:12 +0300

    New Daily Calorie Screen

--- PL/DailyCalorieScreen.Designer.cs
+++ PL/DailyCalorieScreen.Designer.cs
@@ -28,19 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button2 = new Button();
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            listBoxTodaysMeals = new ListBox();
+            label2 = new Label();
+            comboBoxActivityLevel = new ComboBox();
+            label1 = new Label();
+            label3 = new Label();
+            labelNeed = new Label();
+            labelTotal = new Label();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Location = new Point(168, 838);
+            button2.Name = "button2";
+            button2.Size = new Size(1594, 10);
+            button2.TabIndex = 18;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Location = new Point(120, 44);
+            button1.Name = "button1";
+            button1.Size = new Size(1594, 10);
+            button1.TabIndex = 19;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(listBoxTodaysMeals);
+            groupBox1.Font = new Font("Comic Sans MS", 13.875F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            groupBox1.Location = new Point(980, 79);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(799, 712);
+            groupBox1.TabIndex = 17;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Today's Meals Total Calories";
+            // 
+            // listBoxTodaysMeals
+            // 
+            listBoxTodaysMeals.FormattingEnabled = true;
+            listBoxTodaysMeals.ItemHeight = 52;
+            listBoxTodaysMeals.Location = new Point(37, 86);
+            listBoxTodaysMeals.Name = "listBoxTodaysMeals";
+            listBoxTodaysMeals.Size = new Size(716, 576);
+            listBoxTodaysMeals.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Comic Sans MS", 13.875F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label2.Location = new Point(202, 611);
+            label2.Name = "label2";
+            label2.Size = new Size(398, 52);
+            label2.TabIndex = 15;
+            label2.Text = "Today's Calorie Need:";
+            // 
+            // comboBoxActivityLevel
+            // 
+            comboBoxActivityLevel.FormattingEnabled = true;
+            comboBoxActivityLevel.Location = new Point(246, 296);
+            comboBoxActivityLevel.Name = "comboBoxActivityLevel";
+            comboBoxActivityLevel.Size = new Size(354, 40);
+            comboBoxActivityLevel.TabIndex = 14;
+            comboBoxActivityLevel.SelectedIndexChanged += comboBoxActivityLevel_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 13.875F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Location = new Point(294, 220);
+            label1.Name = "label1";
+            label1.Size = new Size(266, 51);
+            label1.TabIndex = 13;
+            label1.Text = "Activity Level";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Comic Sans MS", 13.875F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label3.Location = new Point(76, 739);
+            label3.Name = "label3";
+            label3.Size = new Size(524, 52);
+            label3.TabIndex = 15;
+            label3.Text = "Today's Meals Total Calories:";
+            // 
+            // labelNeed
+            // 
+            labelNeed.AutoSize = true;
+            labelNeed.Font = new Font("Comic Sans MS", 13.875F);
+            labelNeed.Location = new Point(635, 611);
+            labelNeed.Name = "labelNeed";
+            labelNeed.Size = new Size(64, 52);
+            labelNeed.TabIndex = 20;
+            labelNeed.Text = "---";
+            // 
+            // labelTotal
+            // 
+            labelTotal.AutoSize = true;
+            labelTotal.Font = new Font("Comic Sans MS", 13.875F);
+            labelTotal.Location = new Point(635, 739);
+            labelTotal.Name = "labelTotal";
+            labelTotal.Size = new Size(64, 52);
+            labelTotal.TabIndex = 21;
+            labelTotal.Text = "---";
             // 
             // DailyCalorieScreen
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1168, 558);
+            ClientSize = new Size(1898, 893);
+            Controls.Add(labelTotal);
+            Controls.Add(labelNeed);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBoxActivityLevel);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(5);
             Name = "DailyCalorieScreen";
             Text = "DailyCalorieScreen";
+            Load += DailyCalorieScreen_Load;
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private ComboBox comboBoxActivityLevel;
+        private Label label1;
+        private ListBox listBoxTodaysMeals;
+        private Label label3;
+        private Label labelNeed;
+        private Label labelTotal;
     }
 }
--- PL/DailyCalorieScreen.cs
+++ PL/DailyCalorieScreen.cs
@@ -1,20 +1,148 @@
-﻿using System;
+﻿using BLL;
+using DAL.Repositories.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Models.Concrete;
+using Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+
 
 namespace PL
 {
     public partial class DailyCalorieScreen : Form
     {
-        public DailyCalorieScreen()
+        public DailyCalorieScreen(User user)
         {
             InitializeComponent();
+            _user = user;
+            mealSummaryService = new();
+            foodService = new();
+            mealService = new();
+            userDetailService = new();
+            userService = new();
+
+
+
+        }
+
+        UserDetailService userDetailService;
+        UserService userService;
+        MealService mealService;
+        FoodService foodService;
+        MealSummaryService mealSummaryService;
+        User _user;
+
+        private void DailyCalorieScreen_Load(object sender, EventArgs e)
+        {
+            foreach (ActivityLevel level in Enum.GetValues(typeof(ActivityLevel)))
+            {
+                comboBoxActivityLevel.Items.Add(level);
+            }
+
+            List<MealSummary> mealSummaryList = new List<MealSummary>();
+
+            foreach (MealSummary mealSummary in mealSummaryService.GetAll())
+            {
+                if (mealSummary.UserID == _user.UserID)
+                {
+                    mealSummaryList.Add(mealSummary);
+                }
+
+            }
+
+            List<int> ints = new List<int>();
+
+            foreach (MealSummary mealSummary in mealSummaryList)
+            {
+                int mealID;
+                mealID = mealSummary.MealID;
+                ints.Add(mealID);
+            }
+
+            foreach (int i in ints)
+            {
+                listBoxTodaysMeals.Items.Add(mealService.GetById(i));
+
+            }
+
+            List<Meal> meal2 = new List<Meal>();
+            foreach (Meal meal in listBoxTodaysMeals.Items)
+            {
+                if (meal2.Contains(meal) == false)
+                {
+                    meal2.Add(meal);
+                }
+            }
+
+            listBoxTodaysMeals.Items.Clear();
+
+            foreach (Meal meal in meal2)
+            {
+                listBoxTodaysMeals.Items.Add(meal);
+            }
+
+            double totalCalorie2 = 0;
+            foreach (Meal meal in listBoxTodaysMeals.Items)
+            {
+                totalCalorie2 += meal.TotalCalorie;
+            }
+
+            labelTotal.Text = totalCalorie2.ToString();
+        }
+
+
+
+        public double CalculateBmr()
+        {
+
+            double bmr = 0;
+            UserDetail userDetail = userDetailService.GetById(_user.UserID);
+            if (userDetail.Gender == Gender.Male)
+            {
+                bmr = 66.5 + (13.75 * userDetail.Weight) + (5 * userDetail.Height) - (6.77 * userDetail.Age);
+                return bmr;
+            }
+            else
+            {
+                bmr = 655.1 + (9.56 * userDetail.Weight) + (1.85 * userDetail.Height) - (4.67 * userDetail.Age);
+                return bmr;
+            }
+            return bmr;
+        }
+
+        private void comboBoxActivityLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxActivityLevel.SelectedIndex == 0)
+            {
+                labelNeed.Text = (CalculateBmr() * 1.0).ToString();
+            }
+            if (comboBoxActivityLevel.SelectedIndex == 1)
+            {
+                labelNeed.Text = (CalculateBmr() * 1.2).ToString();
+            }
+            if (comboBoxActivityLevel.SelectedIndex == 2)
+            {
+                labelNeed.Text = (CalculateBmr() * 1.3).ToString();
+            }
+            if (comboBoxActivityLevel.SelectedIndex == 3)
+            {
+                labelNeed.Text = (CalculateBmr() * 1.4).ToString();
+            }
+            if (comboBoxActivityLevel.SelectedIndex == 4)
+            {
+                labelNeed.Text = (CalculateBmr() * 1.5).ToString();
+            }
         }
     }
 }
